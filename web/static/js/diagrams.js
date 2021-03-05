a12ccab6df6f5e49538fd38248d3fc7674e39f91
tessier <tessier@luxeys.com>
2021-03-05 23:35:21 +0900

    Help Side menu

--- web/static/js/diagrams.js
+++ web/static/js/diagrams.js
@@ -1,30 +1,129 @@
 jQuery(document).ready(function () {
 
-    var example_1_btn = jQuery('#example_1_btn');
-    var example_2_btn = jQuery('#example_2_btn');
-    var example_3_btn = jQuery('#example_3_btn');
-    var more_example_btn = jQuery('#more_example_btn');
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/clouds");
+    editor.session.setMode("ace/mode/python");
 
-    var diagrams_form = jQuery('#diagrams_form');
-
-    var diagrams_data_textarea = jQuery('#diagrams_data');
-
-    function InsertDiagramsText(text) {
-        diagrams_data_textarea.val(text);
-        diagrams_form.submit();
-    }
-
-    example_1_btn.click(function(){
-      InsertDiagramsText(jQuery("#example_1_text").html().trim());
+    $("#btn_1").click(function(){
+      editor.setValue(example_1.trim());
+      $("#diagrams_data").val(editor.getSession().getValue());
+      $("#diagrams_form").submit();
     });
-    example_2_btn.click(function(){
-      InsertDiagramsText(jQuery("#example_2_text").html().trim());
+    $("#btn_2").click(function(){
+      var text = example_2.trim()
+      editor.setValue(text);
+      $("#diagrams_data").val(editor.getSession().getValue());
+      $("#diagrams_form").submit();
     });
-    example_3_btn.click(function(){
-      InsertDiagramsText(jQuery("#example_3_text").html().trim());
+    $("#btn_3").click(function(){
+      var text = example_3.trim()
+      editor.setValue(text);
+      $("#diagrams_data").val(editor.getSession().getValue());
+      $("#diagrams_form").submit();
     });
-    more_example_btn.click(function(){
+    $("#btn_more").click(function(){
       window.open("https://diagrams.mingrammer.com/docs/getting-started/examples", "_blank");
     });
+    $("#build").click(function(){
+      $("#diagrams_data").val(editor.getSession().getValue());
+      $("#diagrams_form").submit();
+    });
 
+    $("#open_help").click(function(){
+      document.getElementById("mySidenav").style.width = "30%";
+      document.getElementById("main").style.marginLeft = "30%";
+      $("#close_help").show();
+      $("#open_help").hide();
+    });
+
+    $("#close_help").click(function(){
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+      $("#open_help").show();
+      $("#close_help").hide();
+    });
 });
+
+var example_1 =`
+  from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+with Diagram("Grouped Workers", show=False, direction="TB"):
+    ELB("lb") >> [EC2("worker1"),
+                  EC2("worker2"),
+                  EC2("worker3"),
+                  EC2("worker4"),
+                  EC2("worker5")] >> RDS("events")`
+
+var example_2 =`
+  from diagrams import Cluster, Diagram
+from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub
+from diagrams.gcp.compute import AppEngine, Functions
+from diagrams.gcp.database import BigTable
+from diagrams.gcp.iot import IotCore
+from diagrams.gcp.storage import GCS
+
+with Diagram("Message Collecting", show=False):
+    pubsub = PubSub("pubsub")
+
+    with Cluster("Source of Data"):
+        [IotCore("core1"),
+         IotCore("core2"),
+         IotCore("core3")] >> pubsub
+
+    with Cluster("Targets"):
+        with Cluster("Data Flow"):
+            flow = Dataflow("data flow")
+
+        with Cluster("Data Lake"):
+            flow >> [BigQuery("bq"),
+                     GCS("storage")]
+
+        with Cluster("Event Driven"):
+            with Cluster("Processing"):
+                flow >> AppEngine("engine") >> BigTable("bigtable")
+
+            with Cluster("Serverless"):
+                flow >> Functions("func") >> AppEngine("appengine")
+
+    pubsub >> flow`
+
+var example_3 =`
+  from diagrams import Cluster, Diagram
+from diagrams.onprem.analytics import Spark
+from diagrams.onprem.compute import Server
+from diagrams.onprem.database import PostgreSQL
+from diagrams.onprem.inmemory import Redis
+from diagrams.onprem.aggregator import Fluentd
+from diagrams.onprem.monitoring import Grafana, Prometheus
+from diagrams.onprem.network import Nginx
+from diagrams.onprem.queue import Kafka
+
+with Diagram("Advanced Web Service with On-Premise", show=False):
+    ingress = Nginx("ingress")
+
+    metrics = Prometheus("metric")
+    metrics << Grafana("monitoring")
+
+    with Cluster("Service Cluster"):
+        grpcsvc = [
+            Server("grpc1"),
+            Server("grpc2"),
+            Server("grpc3")]
+
+    with Cluster("Sessions HA"):
+        master = Redis("session")
+        master - Redis("replica") << metrics
+        grpcsvc >> master
+
+    with Cluster("Database HA"):
+        master = PostgreSQL("users")
+        master - PostgreSQL("slave") << metrics
+        grpcsvc >> master
+
+    aggregator = Fluentd("logging")
+    aggregator >> Kafka("stream") >> Spark("analytics")
+
+    ingress >> grpcsvc >> aggregator`
