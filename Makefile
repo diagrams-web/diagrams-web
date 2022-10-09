ea5e8e0be089ec110bce97372e47abb76e8bcdce
tessier <tessier@luxeys.com>
2022-10-09 13:59:43 +0900

    [Bump] xtd version to 0.22.0.5 + alpine to 3.16

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 # makefile
 
 build:
-		docker-compose up --build
+		docker compose up --build
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env ash
 
+# generate help menu template
 python3 helpers.py
 
+# start the app
 python3 app.py
