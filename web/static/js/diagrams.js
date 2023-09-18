6625da92fbdada1d380da4e00e9276736d1fe58b
luciluci666 <gerroma01@gmail.com>
2023-09-18 15:20:01 +0300

    Add zoom and fix deps

--- web/static/js/diagrams.js
+++ web/static/js/diagrams.js
@@ -1,5 +1,63 @@
 jQuery(document).ready(function () {
 
+  function imageZoom(imgID, resultID) {
+    var img, lens, result, cx, cy;
+    img = document.getElementById(imgID);
+    result = document.getElementById(resultID);
+    /*create lens:*/
+    lens = document.createElement("DIV");
+    lens.setAttribute("class", "img-zoom-lens");
+    /*insert lens:*/
+    img.parentElement.insertBefore(lens, img);
+    /*calculate the ratio between result DIV and lens:*/
+    cx = result.offsetWidth / lens.offsetWidth / 3;
+    cy = result.offsetHeight / lens.offsetHeight /3;
+    console.log(img.height)
+    /*set background properties for the result DIV:*/
+    result.style.backgroundImage = "url('" + img.src + "')";
+    result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+    /*execute a function when someone moves the cursor over the image, or the lens:*/
+    lens.addEventListener("mousemove", moveLens);
+    img.addEventListener("mousemove", moveLens);
+    /*and also for touch screens:*/
+    lens.addEventListener("touchmove", moveLens);
+    img.addEventListener("touchmove", moveLens);
+    function moveLens(e) {
+      var pos, x, y;
+      /*prevent any other actions that may occur when moving over the image:*/
+      e.preventDefault();
+      /*get the cursor's x and y positions:*/
+      pos = getCursorPos(e);
+      /*calculate the position of the lens:*/
+      x = pos.x - (lens.offsetWidth / 2);
+      y = pos.y - (lens.offsetHeight / 2);
+      /*prevent the lens from being positioned outside the image:*/
+      if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+      if (x < 0) {x = 0;}
+      if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+      if (y < 0) {y = 0;}
+      /*set the position of the lens:*/
+      lens.style.left = x + "px";
+      lens.style.top = y + "px";
+      /*display what the lens "sees":*/
+      result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    }
+
+    function getCursorPos(e) {
+      var a, x = 0, y = 0;
+      e = e || window.event;
+      /*get the x and y positions of the image:*/
+      a = img.getBoundingClientRect();
+      /*calculate the cursor's x and y coordinates, relative to the image:*/
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+      /*consider any page scrolling:*/
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return {x : x, y : y};
+    }
+  }
+
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/clouds");
     editor.session.setMode("ace/mode/python");
@@ -39,8 +97,11 @@ jQuery(document).ready(function () {
         })
         .done(function(result) {
             if(result.pic_name){
-              $("#render").html('<img src="static/diagrams/'+result.pic_name+'" alt="'+result.pic_name+'" />');
+              $("#render").html('<img id="myimage" src="static/diagrams/'+result.pic_name+'" alt="'+result.pic_name+'" />');
+              $("#donwload").attr('href', 'static/diagrams/' + result.pic_name)
+              $("#donwload").attr('download', result.pic_name)
               $("#render").removeClass('error');
+              imageZoom("myimage", "myresult");
             }else if (result.error){
               $("#render").html('<h4>ERROR</h4><pre><code>'+result.error+'</code></pre>');
               $("#render").addClass('error');
